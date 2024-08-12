8bddf6cadad3420ca91fd021b6e0b68183e18c1c
Marek Zelinka <mzelinka17@gmail.com>
2024-08-12 20:15:37 +0200

    use JSX based routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 import ErrorPage from './error-page.jsx'
 import './index.css'
 import Contact, {
@@ -8,50 +13,41 @@ import Contact, {
   loader as contactLoader,
 } from './routes/contact.jsx'
 import { action as destroyAction } from './routes/destroy.jsx'
-import EditContact, { action as contactEditAction } from './routes/edit.jsx'
+import EditContact, { action as editAction } from './routes/edit.jsx'
 import Index from './routes/index.jsx'
 import Root, {
   action as rootAction,
   loader as rootLoader,
 } from './routes/root.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            index: true,
-            element: <Index />,
-          },
-          {
-            path: 'contacts/:contactId',
-            element: <Contact />,
-            loader: contactLoader,
-            action: contactAction,
-          },
-          {
-            path: 'contacts/:contactId/edit',
-            element: <EditContact />,
-            loader: contactLoader,
-            action: contactEditAction,
-          },
-          {
-            path: 'contacts/:contactId/destroy',
-            action: destroyAction,
-            errorElement: <div>Oops! There was an error.</div>,
-          },
-        ],
-      },
-    ],
-  },
-])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route path="contacts/:contactId/destroy" action={destroyAction} />
+      </Route>
+    </Route>,
+  ),
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
