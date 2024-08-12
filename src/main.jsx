ff13bc96595f6a33d63f17d13b2dc7bce123421e
Marek Zelinka <mzelinka17@gmail.com>
2024-08-12 09:52:14 +0200

    handle 404 errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './error-page.jsx'
 import './index.css'
-import Root from './routes/root'
+import Root from './routes/root.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ])
 
